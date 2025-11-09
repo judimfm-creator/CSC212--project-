8db7cd41ca4f5e80a0bc54582c90e93f01b5f668
Ghala100288 <ghala3772@gmail.com>
2025-11-09 22:02:24 +0300

    Edit Review

--- Review.java
+++ Review.java
@@ -1,9 +1,9 @@
 public class Review {
-    int reviewId;
-    int productID;
-    int customerID;
-    int rating;
-    String comment;
+    private int reviewId;
+    private int productID;
+    private int customerID;
+    private int rating;
+    private String comment;
 
     public Review() {
         this.reviewId = 0;
@@ -21,52 +21,28 @@ public class Review {
         this.comment = comment;
     }
 
-    public int getReviewId() {
-        return reviewId;
-    }
+    public int getReviewId() { return reviewId; }
+    public void setReviewId(int reviewId) { this.reviewId = reviewId; }
 
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
-    }
+    public int getProduct() { return productID; }
+    public void setProduct(int product) { this.productID = product; }
 
-    public int getProduct() {
-        return productID;
-    }
+    public int getCustomer() { return customerID; }
+    public void setCustomer(int customer) { this.customerID = customer; }
 
-    public void setProduct(int product) {
-        this.productID = product;
-    }
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
 
-    public int getCustomer() {
-        return customerID;
-    }
-
-    public void setCustomer(int customer) {
-        this.customerID = customer;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
 
     @Override
     public String toString() {
-        return "\nReview{" + "reviewId=" + reviewId + ", product=" + productID 
-                + ", customer=" + customerID + ", rating=" + rating + ", comment=" + comment + '}';
+        return "Review ID: " + reviewId +
+               ", Product: " + productID +
+               ", Customer: " + customerID +
+               ", Rating: " + rating +
+               ", Comment: " + comment;
     }
-    
-    
-    
 }
+
